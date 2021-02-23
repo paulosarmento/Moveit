5d0ff9cbb2e725c503861430ae2a5ab091780061
paulosarmento <paulo.cesar.sarmento@hotmail.com>
2021-02-23 18:54:16 -0300

    Banco de dados

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,13 +1,9 @@
+import "reflect-metadata";
 import express from "express";
 import "./database";
+import { router } from "./routes";
 const app = express();
-
-app.get("/users", (req, res) => {
-  return res.send({ message: "Hello world" });
-});
-
-app.post("/", (req, res) => {
-  return res.json({ message: "Os dados foram salvos com sucesso!" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
